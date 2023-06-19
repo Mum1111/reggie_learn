195527c6e4233eddb8d6b8cf109efec923570c0d
施宏锦 <543528285@qq.com>
2023-06-19 10:38:00 +0800

    feat: 完成分页查询套餐

--- src/main/java/com/mumi/reggie/controller/SetmealController.java
+++ src/main/java/com/mumi/reggie/controller/SetmealController.java
@@ -4,12 +4,18 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.mumi.reggie.common.R;
 import com.mumi.reggie.dto.SetmealDto;
+import com.mumi.reggie.entity.Category;
 import com.mumi.reggie.entity.Setmeal;
+import com.mumi.reggie.service.CategoryService;
 import com.mumi.reggie.service.SetmealDishService;
 import com.mumi.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/setmeal")
@@ -21,6 +27,9 @@ public class SetmealController {
     @Autowired
     private SetmealDishService setmealDishService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     @PostMapping
     public R<String> save(@RequestBody SetmealDto setmealDto) {
         setmealService.saveWithDish(setmealDto);
@@ -28,14 +37,38 @@ public class SetmealController {
     }
 
     @GetMapping("/page")
-    public R<Page<Setmeal>> page(int page, int pageSize, String name){
+    public R<Page<SetmealDto>> page(int page, int pageSize, String name){
         Page<Setmeal> pageInfo = new Page<>(page, pageSize);
+        Page<SetmealDto> dtoPage = new Page<>();
 
         LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.like(name != null, Setmeal::getName, name);
         queryWrapper.orderByDesc(Setmeal::getUpdateTime);
 
         setmealService.page(pageInfo, queryWrapper);
-        return R.success(pageInfo);
+
+        BeanUtils.copyProperties(pageInfo, dtoPage, "records");
+
+        List<Setmeal> records = pageInfo.getRecords();
+
+        List<SetmealDto> setmealDtoList = records.stream().map(item -> {
+            SetmealDto setmealDto = new SetmealDto();
+
+            BeanUtils.copyProperties(item, setmealDto);
+
+            Long categoryId = item.getCategoryId();
+
+            Category category = categoryService.getById(categoryId);
+
+            if (category != null) {
+                setmealDto.setCategoryName(category.getName());
+            }
+            return setmealDto;
+        }).collect(Collectors.toList());
+
+        dtoPage.setRecords(setmealDtoList);
+
+
+        return R.success(dtoPage);
     }
 }
