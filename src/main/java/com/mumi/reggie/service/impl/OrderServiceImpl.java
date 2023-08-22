1d06dc421c543196810156b406a6b2e0837453a0
施宏锦 <543528285@qq.com>
2023-08-22 18:08:00 +0800

    feat: 写入订单明细表完成

--- src/main/java/com/mumi/reggie/service/impl/OrderServiceImpl.java
+++ src/main/java/com/mumi/reggie/service/impl/OrderServiceImpl.java
@@ -1,23 +1,22 @@
 package com.mumi.reggie.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.mumi.reggie.common.BaseContext;
 import com.mumi.reggie.common.CustomException;
-import com.mumi.reggie.entity.AddressBook;
-import com.mumi.reggie.entity.Orders;
-import com.mumi.reggie.entity.ShoppingCart;
-import com.mumi.reggie.entity.User;
+import com.mumi.reggie.entity.*;
 import com.mumi.reggie.mapper.OrdersMapper;
-import com.mumi.reggie.service.AddressBookService;
-import com.mumi.reggie.service.OrdersService;
-import com.mumi.reggie.service.ShoppingCartService;
-import com.mumi.reggie.service.UserService;
+import com.mumi.reggie.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderServiceImpl extends ServiceImpl<OrdersMapper, Orders> implements OrdersService {
@@ -30,6 +29,8 @@ public class OrderServiceImpl extends ServiceImpl<OrdersMapper, Orders> implemen
 
     @Autowired
     private AddressBookService addressBookService;
+
+    private OrderDetailService orderDetailService;
 
     @Override
     @Transactional
@@ -57,10 +58,43 @@ public class OrderServiceImpl extends ServiceImpl<OrdersMapper, Orders> implemen
             throw new CustomException("地址信息为空，不能下单");
         }
 
+        long orderId = IdWorker.getId();
+
+        AtomicInteger amount = new AtomicInteger(0);
+        // 遍历购物车，计算总金额
+        List<OrderDetail> orderDetails = shoppingCartList.stream().map((item) -> {
+            OrderDetail orderDetail = new OrderDetail();
+            orderDetail.setOrderId(orderId);
+            orderDetail.setNumber(item.getNumber());
+            orderDetail.setDishFlavor(item.getDishFlavor());
+            orderDetail.setDishId(item.getDishId());
+            orderDetail.setSetmealId(item.getSetmealId());
+            orderDetail.setName(item.getName());
+            orderDetail.setImage(item.getImage());
+            orderDetail.setAmount(item.getAmount());
+            amount.addAndGet(item.getAmount().multiply(new BigDecimal(item.getNumber())).intValue());
+            return orderDetail;
+        }).collect(Collectors.toList());
 
         // 向订单表中插入一条数据
+        orders.setId(orderId);
+        orders.setOrderTime(LocalDateTime.now());
+        orders.setCheckoutTime(LocalDateTime.now());
+        orders.setStatus(2);
+        orders.setAmount(new BigDecimal(amount.get()));//总金额
+        orders.setUserId(userId);
+        orders.setNumber(String.valueOf(orderId));
+        orders.setUserName(user.getName());
+        orders.setConsignee(addressBook.getConsignee());
+        orders.setPhone(addressBook.getPhone());
+        orders.setAddress((addressBook.getProvinceName() == null ? "" : addressBook.getProvinceName())
+                + (addressBook.getCityName() == null ? "" : addressBook.getCityName())
+                + (addressBook.getDistrictName() == null ? "" : addressBook.getDistrictName())
+                + (addressBook.getDetail() == null ? "" : addressBook.getDetail()));
 
+        this.save(orders);
         // 向订单明细表中插入多条数据
+        orderDetailService.saveBatch(orderDetails);
 
         // 清空购物车数据
     }
